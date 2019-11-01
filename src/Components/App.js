6bc97a151debe748c1ae5a4c844981f1bf6f6e2b
Robin7374 <7374robin@gmail.com>
2019-11-01 13:08:56 -0600

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import { Header, Footer } from './Layouts';
+import Exercise from './Exercise';
+
+export default class extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <Exercise />
+        <Footer />
+      </Fragment>
+    );
+  }
+}
---
+++ src/Components/Layouts/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography } from '@material-ui/core/';
+
+export default props => (
+  <AppBar position='static'>
+    <Toolbar></Toolbar>
+  </AppBar>
+);
